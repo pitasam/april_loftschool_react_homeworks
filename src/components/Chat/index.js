7d1f60fb58bde5c8cdaed1d85d63565da0e9522f
Margarita Samsonova <pitasam@yandex.ru>
2018-04-10 15:23:48 +0300

    Второе дз (один тест не проходит)

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,1 +1,54 @@
-export { default } from './Chat';
+import React, { Component } from 'react';
+import Message from './../Message';
+import './Chat.css';
+
+class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: [],
+            messageInput: '',
+        };
+    }
+
+    changeInputMessage = (event) => {
+        // значение input сохраняем в messageInput
+        let  value;
+
+        value  = event.target.value;
+        this.setState(({ messageInput }) => ({messageInput: value }));
+    };
+
+    sendMessageOnEnter = (event) => {
+        // нажат enter
+        // сохраняем сообщение в массив
+        // переменную messageInput обнуляем в пустую строку
+        if (event.key === 'Enter') {
+            const mes = this.state.messageInput;
+
+            this.setState((state) => ( this.state.messages.push({'text': mes})));
+            this.setState(({ messageInput }) => ({'messageInput': ''}));
+        }
+    };
+
+    render() {
+        const { messages } = this.state;
+        const { messageInput } = this.state;
+
+        return (
+            <div className="chat">
+                <Message messages={ messages } />
+
+                <input className="input-message"
+                       onChange={this.changeInputMessage}
+                       onKeyPress={this.sendMessageOnEnter}
+                       value={ messageInput }
+                       type="text"/>
+
+            </div>
+        )
+    }
+}
+
+export default Chat;
+
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,17 @@
-export { default } from './Message';
+import React, { Component } from 'react';
+import './Message.css';
+
+class Message extends Component {
+
+    render() {
+        const { messages } = this.props;
+
+        return (
+            <ul>
+                {messages.map(item => <li className="message">{ item.text }</li>)}
+            </ul>
+        )
+    }
+}
+
+export default Message;
