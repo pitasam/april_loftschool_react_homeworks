0d53343b7e0c66cd9185dd40cdc8b536b47917f3
Margarita Samsonova <pitasam@yandex.ru>
2018-04-23 14:20:14 +0300

    Функционал работает, тесты проходят

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,36 @@ import { AuthHOC } from 'components/AuthorizeProvider';
 import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
+    state = {
+        login: '',
+        password: ''
+    };
+
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+    handleClick = () => {
+      const { authorizeUser } = this.props;
+
+        authorizeUser(this.state.login, this.state.password);
+    };
+
   render() {
     const { isAuthorized } = this.props;
+    const formLogin = (
+        <div>
+          <div>
+            <input name="login" onChange={this.handleChange}/>
+            <input name="password" onChange={this.handleChange}/>
+          </div>
+          <button onClick={this.handleClick}>
+            Submit
+          </button>
+        </div>
+    );
 
-    return isAuthorized ? <Redirect to="/" /> : null;
+    return isAuthorized ? <Redirect to="/" /> : formLogin;
   }
 }
 
--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,9 +1,12 @@
 import React, { PureComponent } from 'react';
+import { Redirect, Route } from 'react-router-dom';
 import { AuthHOC } from 'components/AuthorizeProvider';
 
 class PrivateRoute extends PureComponent {
   render() {
-    return null;
+    const { isAuthorized, component, path } = this.props;
+
+    return isAuthorized ? <Route path={path} component={component} /> : <Redirect to="/login" />;
   }
 }
 
